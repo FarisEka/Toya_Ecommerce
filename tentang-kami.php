4d35b9bb7024eec34cb75ddfb6e0b6e6acbce57f
Faris <fariseka3@gmail.com>
2025-04-30 16:48:08 +0700

    add halaman about us

---
+++ tentang-kami.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rizz Cloth || Tentang Kami</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php require "navbar.php"; ?>
+
+        <!-- Banner -->
+        <div class="container-fluid banner-produk d-flex align-items-center">
+        <div class="container">
+            <h1 class="text-white text-center">Tentang Kami</h1>
+        </div>
+    </div>
+
+    <div class="container-fluid py-5">
+        <div class="container fs-5 text-center">
+            <p> Rizz Cloth adalah brand fashion lokal yang hadir untuk memenuhi kebutuhan gaya berpakaian modern dan berkualitas tinggi dengan harga yang tetap terjangkau. Kami percaya bahwa setiap orang berhak tampil percaya diri dan stylish dalam setiap aktivitasnya, tanpa harus menguras dompet.
+
+Sejak didirikan, Rizz Cloth berkomitmen menghadirkan berbagai pilihan produk fashion pria dan wanita seperti sweater, hoodie, jaket bomber, tas, hingga aksesoris, yang selalu mengikuti tren terkini. Setiap produk kami dibuat dengan bahan pilihan dan melalui proses quality control yang ketat, demi memastikan kenyamanan dan kepuasan pelanggan.
+
+Kami juga terus berinovasi dalam layanan, mulai dari kemudahan belanja online, respons cepat customer service, hingga sistem pengiriman yang aman dan tepat waktu. Dengan semangat #TampilPercayaDiri, Rizz Cloth akan terus berkembang dan menjadi bagian dari gaya hidup masyarakat Indonesia.</p>
+        </div>
+    </div>
+
+
+    <!-- footer -->
+    <?php require "footer.php"; ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+    <script src="fontawesome/js/all.min.js"></script>
+
+</body>
+
+</html>
